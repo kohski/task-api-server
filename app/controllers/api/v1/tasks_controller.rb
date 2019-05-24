ffb04b0396577c03b05f92862d88344083d4b9dc
木村　幸介 <kosukekimura0528@gmail.com>
2019-05-24 13:09:09 +0900

    image passing for the time being

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -27,18 +27,18 @@ module Api
       def create
         task = Task.new(set_task);
 
-        if !params[:picture].empty?
-          image_match = params[:picture].match(/^data:(.*?);(?:.*?),(.*)$/)
-          mime_type, encoded_image = image_match.captures
-          extension = mime_type.split('/').second
-          decoded_image = Base64.decode64(encoded_image)
-          filename = "picture#{task.id}.#{extension}"
-          image_path = "#{Rails.root}/tmp/storage/#{filename}"
-          File.open(image_path, 'wb') do |f|
-            f.write(decoded_image)
-          end
-          task.picture.attach({ io: File.open(image_path), filename: filename, content_type: mime_type })  
-        end
+        # if !params[:picture].empty?
+        #   image_match = params[:picture].match(/^data:(.*?);(?:.*?),(.*)$/)
+        #   mime_type, encoded_image = image_match.captures
+        #   extension = mime_type.split('/').second
+        #   decoded_image = Base64.decode64(encoded_image)
+        #   filename = "picture#{task.id}.#{extension}"
+        #   image_path = "#{Rails.root}/tmp/storage/#{filename}"
+        #   File.open(image_path, 'wb') do |f|
+        #     f.write(decoded_image)
+        #   end
+        #   task.picture.attach({ io: File.open(image_path), filename: filename, content_type: mime_type })  
+        # end
 
         if task.save
           render json: {
